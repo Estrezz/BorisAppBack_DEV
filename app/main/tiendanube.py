870acdc7bf6030714d60463dbf0d04f5602f26d7
Esteban <erezzoni@yandex.com>
2021-04-06 11:27:16 -0300

    instala 6

--- app/main/tiendanube.py
+++ app/main/tiendanube.py
@@ -116,11 +116,11 @@ def autorizar_tiendanube(codigo):
         
         #flash('Store {}'.format(store))
         if Company.query.filter_by(store_id=store).first_or_404 != 404:
-            unaEmpresa = Customer.query.get(store)
+            unaEmpresa = Company.query.filter_by(store_id=store).first()
             flash('Ya existe la empresa {} - {}'.format(unaEmpresa, respuesta))
-            #unaEmpresa.platform_token_type = respuesta['token_type']
-            #unaEmpresa.platform_access_token = respuesta['access_token']
-            return unaEmpresa.store_name
+            unaEmpresa.platform_token_type = respuesta['token_type']
+            unaEmpresa.platform_access_token = respuesta['access_token']
+            
         else: 
             empresa = traer_datos_tiendanube(store, respuesta['token_type'],respuesta['access_token'] )
             unaEmpresa = Company(
