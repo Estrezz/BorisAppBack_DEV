de63ed8113824b6738935bbe276baeba0eafc5a9
Esteban <erezzoni@yandex.com>
2021-12-16 16:36:40 -0300

    incializacion de datos

--- Inicializar_sender_name.py
+++ Inicializar_sender_name.py
@@ -1,6 +1,7 @@
 from app import db, create_app
-from app.models import Company
+from app.models import Company, CONF_boris
 from flask import current_app
+from app.main.interfaces import actualiza_empresa_JSON
 
 
 app=create_app()
@@ -18,8 +19,21 @@ with app.app_context():
             mail = x.communication_email
             x.communication_email_name =  mail.split("@")[0]
             print ('sender :' + x.communication_email_name +' -'+ mail)
+       
+
+        actualiza_empresa_JSON(x, 'portal_empresa', x.store_name, 'textos')
+        actualiza_empresa_JSON(x, 'portal_titulo', 'Cambios y Devoluciones', 'textos')
+        
+       
+
+        empresa = CONF_boris.query.filter_by(store=x.store_id).first()
+        empresa.portal_empresa = x.store_name
+        empresa.portal_texto = 'Cambios y Devoluciones'
+        
 
         db.session.commit()
+
+
         print ('Finalizado '+str(x.store_id)+' '+x.store_name)
 
                 
